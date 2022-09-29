aae6f8136e8daada49aea9f73314fecf3f07e30e
Mr. Kiedes <akiedes@vtsd.com>
2022-09-29 08:14:34 -0400

    Initial Lab Upload

---
+++ Lab04avst.java
@@ -0,0 +1,35 @@
+// Lab04avst.java
+// The Mortgage Payment Program
+// This the student, starting version of the Lab04a assignment.
+
+
+public class Lab04avst
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Lab04a, Student Version\n");
+
+        double Principal  = 250000;
+        double AnnualRate = 4.85;
+        double Years   = 30;
+        double Months = Years * 12;
+        double monthlyrate = (AnnualRate / 100) / 12;
+        double Numerator = monthlyrate * Math.pow(1 + monthlyrate, Months);
+        double Denominator = Math.pow(1 + monthlyrate, Months) - 1;
+        double C = Numerator / Denominator;
+        double MonthlyPayment = C * Principal;
+
+
+
+
+
+
+
+        System.out.println("Principal"+"    " + "$"+Principal);
+        System.out.println("Annual Rate"+"    " + AnnualRate + "%");
+        System.out.println("Number of years"+"    " + Years);
+
+        System.out.println("Monthly payment"+"    " +"$"+ MonthlyPayment);
+    }
+}
+
